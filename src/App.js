cb7de78127043c508cc675887919879c928767d6
lahcenbont <rachdilahcen2023@gmail.com>
2025-02-01 11:24:35 +0100

    update 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Example from './components/Nav';
+import Home from './Home';
+function App() {
+  return (
+    <div className="App">
+      <Example />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
